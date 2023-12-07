5c6ab22179c375fe07eba5b16b4ddde5e37ccac1
shadowtyw <415389456@qq.com>
2023-12-07 14:28:44 +0800

    autofan
    基于unraid app dynamix.system.autofan，进行修改，将温度获取种类由磁盘改为cpu温度。便于nas diy玩家使用。cpu被动散热勿使用。
    curl "https://raw.githubusercontent.com/chaosk731/dynamix.system.autofan.cpu/master/dynamix.system.autofan.cpu.sh" | /bin/bash

---
+++ dynamix.system.autofan.cpu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#=======================================================================================
+#  Name:  dynamix.system.autofan.cpu
+#=======================================================================================
+#  Description:
+#
+#  app:dynamix.system.autofan(Version 1.6) chang to get cpu temp
+PATH="/tmp/dynamix.system.autofan.cpu"
+ShName="autofan"
+BINPATH="/usr/local/emhttp/plugins/dynamix.system.autofan/scripts"
+
+if [[ -d ${BINPATH} ]]; then 
+  echo ""
+else
+  echo "dynamix.system.autofan dose no exist!"
+  exit 1
+fi
+
+if [[ -d ${PATH} ]]; then 
+  echo ""
+else
+  /usr/bin/mkdir -p ${PATH}
+  cd ${PATH} && /usr/bin/wget -q https://raw.githubusercontent.com/chaosk731/dynamix.system.autofan.cpu/master/autofan
+fi
+
+if [[ -d ${PATH} ]]; then
+  BinMD5=1
+  TmpMD5=1
+  [[ -f ${BINPATH}/${ShName} ]] && BinMD5=$(/usr/bin/md5sum ${BINPATH}/${ShName}|/usr/bin/awk '{print $1}')
+  [[ -f ${PATH}/${ShName} ]] && TmpMD5=$(/usr/bin/md5sum ${PATH}/${ShName}|/usr/bin/awk '{print $1}')
+  if [[ ${BinMD5} != ${TmpMD5} ]];then
+    /usr/bin/cp -af ${PATH}/${ShName} ${PATH}/backup_${ShName} 
+    /usr/bin/cp -af ${PATH}/${ShName} ${BINPATH}/${ShName}
+    /usr/bin/chmod +x ${BINPATH}/${ShName}
+    /usr/local/emhttp/plugins/dynamix.system.autofan/scripts/rc.autofan restart
+  fi
+  echo -e "dynamix.system.autofan.cpu done!\n"
+  echo -e "backup file path : ${PATH}/backup_${ShName}\n"
+fi
